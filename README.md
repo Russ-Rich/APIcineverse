6b585261dc36aef63faad82fa55828021cea89d0
Russ <russ3750@gmail.com>
2023-08-26 18:32:23 -0500

    cleaned up code for 1.2

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # APIcineverse
- APIcineverse is a tool for developers to easily access and work with movie data. It offers straightforward ways to search, sort, and manage a big collection of films.
+
+APIcineverse is a tool for developers to easily access and work with movie data. It offers straightforward ways to search, sort, and manage a big collection of films.
+
+## Quick Recap - Updated on 2023-08-26
+
+1. **`documentation.html`**: Created and added initial instructions for API usage.
+
+2. **`server.js`**:
+
+    - Imports essential Node.js modules: `http`, `fs`, `url`, and `path`.
+    - Handles incoming requests based on URL paths and listens on port 8080.
+
+3. **`log.txt`**: A log file where all incoming HTTP requests, including the request URL and timestamp, are stored.
+
+---
+
+### To test setup
+
+- Run `node server.js` in the terminal.
+- Open a web browser and navigate to `http://localhost:8080/` and `http://localhost:8080/documentation`.
+- Validate logs in `log.txt` file.
--- server.js
+++ server.js
@@ -1,11 +1,49 @@
+// Import necessary modules
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello World\n");
+// Create HTTP server
+const server = http.createServer((request, response) => {
+	// Parse the URL
+	const parsedUrl = url.parse(request.url, true);
+
+	// Log the request
+	fs.appendFile("log.txt", `${new Date().toISOString()} - ${parsedUrl.pathname}\n`, (err) => {
+		if (err) throw err;
+	});
+
+	// Handle /documentation path
+	if (parsedUrl.pathname.includes("documentation")) {
+		fs.readFile(path.join(__dirname, "documentation.html"), "utf8", (err, data) => {
+			if (err) {
+				response.writeHead(404, { "Content-Type": "text/plain" });
+				response.write("404 Not Found\n");
+				response.end();
+				return;
+			}
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.write(data);
+			response.end();
+		});
+	} else {
+		// Handle other paths
+		fs.readFile(path.join(__dirname, "index.html"), "utf8", (err, data) => {
+			if (err) {
+				response.writeHead(404, { "Content-Type": "text/plain" });
+				response.write("404 Not Found\n");
+				response.end();
+				return;
+			}
+			response.writeHead(200, { "Content-Type": "text/html" });
+			response.write(data);
+			response.end();
+		});
+	}
 });
 
+// Listen on port 8080
 server.listen(8080, () => {
 	console.log("Server running on http://localhost:8080/");
 });
